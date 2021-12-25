1c333febabb74b9a8fa9726ab5315f9a67a28682
patrickmcdermott <patrickmcdermott@patricks-mbp.fios-router.home>
2021-12-25 14:44:52 -0500

    deleted comment

--- src/main/java/com/example/notes/services/NoteService.java
+++ src/main/java/com/example/notes/services/NoteService.java
@@ -41,7 +41,7 @@ public class NoteService {
 		
 		//for each note, get id and get category and populte into dto
 		noteList.parallelStream().forEach(a->{
-		//this line returns an optionsal so will have to be fixed as this will break
+		
 				String category = this.categoriesRepository.getById(a.getCategory_id()).getName();
 				noteDTOList.add(new NoteDTO(a.getNote_id(), a.getBody(), category));
 		});
